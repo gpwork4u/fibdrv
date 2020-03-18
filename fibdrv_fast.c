94e4ee28ca0e846a96a9a3063cd97132f4351337
GP <gpwork4u@gmail.com>
2020-03-18 19:39:03 +0800

    Add fib fast doubling

---
+++ fibdrv_fast.c
@@ -0,0 +1,200 @@
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kdev_t.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_AUTHOR("National Cheng Kung University, Taiwan");
+MODULE_DESCRIPTION("Fibonacci engine driver");
+MODULE_VERSION("0.1");
+
+#define DEV_FIBONACCI_NAME "fibonacci"
+
+/* MAX_LENGTH is set to 92 because
+ * ssize_t can't fit the number > 92
+ */
+#define MAX_LENGTH 92
+
+static dev_t fib_dev = 0;
+static struct cdev *fib_cdev;
+static struct class *fib_class;
+static DEFINE_MUTEX(fib_mutex);
+static ktime_t kt;
+
+static long long lpow(long long k, long long exp)
+{
+    long long result = 1;
+    for (long long i = 0; i < exp; i++)
+        result *= k;
+    return result;
+}
+
+static long long fast_fib(long long k)
+{
+    long long a, b;
+    unsigned long long u = 1;
+    a = 0;
+    b = 1;
+    for (int i = sizeof(k) * 8; i >= 1; i--) {
+        long long t1 = a * (2 * b - a);
+        long long t2 = lpow(b, 2) + lpow(a, 2);
+        a = t1;
+        b = t2;
+        if ((k & (u << (i - 1))) > 0) {
+            t1 = a + b;
+            a = b;
+            b = t1;
+        }
+    }
+    return a;
+}
+
+static long long fib_time_proxy(long long k)
+{
+    kt = ktime_get();
+    long long result = fast_fib(k);
+    kt = ktime_sub(ktime_get(), kt);
+    return result;
+}
+
+static int fib_open(struct inode *inode, struct file *file)
+{
+    if (!mutex_trylock(&fib_mutex)) {
+        printk(KERN_ALERT "fibdrv is in use");
+        return -EBUSY;
+    }
+    return 0;
+}
+
+static int fib_release(struct inode *inode, struct file *file)
+{
+    mutex_unlock(&fib_mutex);
+    return 0;
+}
+
+/* calculate the fibonacci number at given offset */
+static ssize_t fib_read(struct file *file,
+                        char *buf,
+                        size_t size,
+                        loff_t *offset)
+{
+    char *kbuf = kmalloc(sizeof(char) * size, GFP_USER);
+    long long result = fib_time_proxy(*offset);
+    snprintf(kbuf, size, "%lld", result);
+    copy_to_user(buf, kbuf, size);
+    return (ssize_t) kt;
+}
+
+/* write operation is skipped */
+static ssize_t fib_write(struct file *file,
+                         const char *buf,
+                         size_t size,
+                         loff_t *offset)
+{
+    return 1;
+}
+
+static loff_t fib_device_lseek(struct file *file, loff_t offset, int orig)
+{
+    loff_t new_pos = 0;
+    switch (orig) {
+    case 0: /* SEEK_SET: */
+        new_pos = offset;
+        break;
+    case 1: /* SEEK_CUR: */
+        new_pos = file->f_pos + offset;
+        break;
+    case 2: /* SEEK_END: */
+        new_pos = MAX_LENGTH - offset;
+        break;
+    }
+
+    if (new_pos > MAX_LENGTH)
+        new_pos = MAX_LENGTH;  // max case
+    if (new_pos < 0)
+        new_pos = 0;        // min case
+    file->f_pos = new_pos;  // This is what we'll use now
+    return new_pos;
+}
+
+const struct file_operations fib_fops = {
+    .owner = THIS_MODULE,
+    .read = fib_read,
+    .write = fib_write,
+    .open = fib_open,
+    .release = fib_release,
+    .llseek = fib_device_lseek,
+};
+
+static int __init init_fib_dev(void)
+{
+    int rc = 0;
+
+    mutex_init(&fib_mutex);
+
+    // Let's register the device
+    // This will dynamically allocate the major number
+    rc = alloc_chrdev_region(&fib_dev, 0, 1, DEV_FIBONACCI_NAME);
+
+    if (rc < 0) {
+        printk(KERN_ALERT
+               "Failed to register the fibonacci char device. rc = %i",
+               rc);
+        return rc;
+    }
+
+    fib_cdev = cdev_alloc();
+    if (fib_cdev == NULL) {
+        printk(KERN_ALERT "Failed to alloc cdev");
+        rc = -1;
+        goto failed_cdev;
+    }
+    cdev_init(fib_cdev, &fib_fops);
+    rc = cdev_add(fib_cdev, fib_dev, 1);
+
+    if (rc < 0) {
+        printk(KERN_ALERT "Failed to add cdev");
+        rc = -2;
+        goto failed_cdev;
+    }
+
+    fib_class = class_create(THIS_MODULE, DEV_FIBONACCI_NAME);
+
+    if (!fib_class) {
+        printk(KERN_ALERT "Failed to create device class");
+        rc = -3;
+        goto failed_class_create;
+    }
+
+    if (!device_create(fib_class, NULL, fib_dev, NULL, DEV_FIBONACCI_NAME)) {
+        printk(KERN_ALERT "Failed to create device");
+        rc = -4;
+        goto failed_device_create;
+    }
+    return rc;
+failed_device_create:
+    class_destroy(fib_class);
+failed_class_create:
+    cdev_del(fib_cdev);
+failed_cdev:
+    unregister_chrdev_region(fib_dev, 1);
+    return rc;
+}
+
+static void __exit exit_fib_dev(void)
+{
+    mutex_destroy(&fib_mutex);
+    device_destroy(fib_class, fib_dev);
+    class_destroy(fib_class);
+    cdev_del(fib_cdev);
+    unregister_chrdev_region(fib_dev, 1);
+}
+
+module_init(init_fib_dev);
+module_exit(exit_fib_dev);
